3683af9fcb9995a1afc8653a3691a55885160021
dodo721 <dodoworkspr@gmail.com>
2024-11-20 21:29:27 +0000

    Test API commands with exec

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const express = require('express');
+const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
+
+app.get('/test', async (req, res) => {
+    const { stdout, stderr } = await exec('ls');
+    res.send(stdout);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
