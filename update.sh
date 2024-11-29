cd70563152e1bd8e36bd3d0918a21363c14b9079
dodo721 <dodoworkspr@gmail.com>
2024-11-29 02:05:16 +0000

    Remove sysadmin files

--- update.sh
+++
@@ -1,9 +0,0 @@
-echo "UPDATING!"
-cd /home/oem/www/
-echo "Git pull..."
-git pull
-echo "Lessc..."
-lessc ./src/style/main.less ./public/style/main.css
-echo "Nginx reload..."
-nginx -s reload
-echo "FINISHED!"
