5dfcadbf34ca3714ca114268b70cd64dc1b95b33
dodo721 <dodoworkspr@gmail.com>
2024-11-29 01:41:32 +0000

    Include less compilation in update

--- update.sh
+++ update.sh
@@ -1,2 +1,9 @@
+echo "UPDATING!"
+cd /home/oem/www/
+echo "Git pull..."
 git pull
+echo "Lessc..."
+lessc ./src/style/main.less ./public/style/main.css
+echo "Nginx reload..."
 nginx -s reload
+echo "FINISHED!"
