d3243b599840fd4a79f6328acf5afbbfdc572908
dodo721 <dodoworkspr@gmail.com>
2024-11-30 03:04:01 +0000

    Marquee update

--- public/js/marquee.js
+++ public/js/marquee.js
@@ -1,6 +1,12 @@
 
 $(() => {
-    $('.marquee').each((i, el) => {
-        
+    $('.marquee').each((idxMarquee, elMarquee) => {
+        const innerOne = document.createElement("div");
+        const innerTwo = document.createElement("div");
+        $(el).append(innerOne);
+        $(innerOne).after(innerTwo);
+        const child = $(el).children(":first");
+        $(child).appendTo(innerOne);
+        $(child).clone().appendTo(innerTwo);
     });
 });
