dcd25f02f8ec3e4cf79ebcd0322682d7303868ce
dodo721 <dodoworkspr@gmail.com>
2024-11-30 03:09:53 +0000

    marquee update

--- public/js/marquee.js
+++ public/js/marquee.js
@@ -2,7 +2,9 @@
 $(() => {
     $('.marquee').each((i, el) => {
         const innerOne = document.createElement("div");
+        innerOne.classList.add("innerOne");
         const innerTwo = document.createElement("div");
+        innerTwo.classList.add("innerTwo");
         $(el).append(innerOne);
         $(innerOne).after(innerTwo);
         const child = $(el).children(":first");
